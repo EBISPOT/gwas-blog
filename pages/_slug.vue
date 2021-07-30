ccb8ccfdb84735ee5190913fa0aef45d1f0a095b
sprintell <abayomi@sprintell.org>
2021-07-30 16:01:28 +0100

    Add index page and slug page

---
+++ pages/_slug.vue
@@ -0,0 +1,85 @@
+<template>
+    <div>
+        <main class="gwas-container">
+            <div class="section section--gray100 gwas-banner">
+                <Header/>
+                <Banner/>
+            </div>
+
+            <section class="gwas-gray20 body-section">
+                <div class="row margin-top-25">
+
+                    <!-- <Sidebar/> -->
+
+                    <div class="column large-10 medium-12 large-offset-1" style="padding: 0 5px;">
+
+                        <div class="row">
+                            <div class="column large-12 medium-12" style="padding: 0 5px;">
+                                <div class="card-2 card-body card-3b gwas-content-area">
+                                    <h2 class="gwas-card-head"> {{ article.title }} 
+                                         <em style="float:right; font-size:11px;"> {{article.date}} By {{ article.author }} </em> </h2>
+
+                                    <div style="margin: 0px; padding:15px; text-align:left; height:">
+                                        <p style="margin-top:0px;">
+
+                                          <nuxt-content :document="article" />
+                                          
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- <div class="row">
+                            <div class="column large-4 medium-4" style="padding: 0 5px;">
+                                <div class="card-2 card-body card-3b my-card">
+                                    <h3 class="gwas-card-head"> Full Data Download </h3>
+
+                                    <div class="gwas-card-body-one centralize">
+                                        <p class="mg-t-0">
+                                            Download a full copy of the GWAS Catalog in spreadsheet format as well as
+                                            current and older versions of the GWAS diagram in SVG format.
+                                        <p>
+                                        <div class="card-footer bd-t-0 pd-t-0">
+                                            <button class="tx-12 tx-uppercase gwas-card-btn"> Download GWAS Catalog
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="column large-4 medium-4" style="padding: 0 5px;">
+                                <div class="card-2 card-body card-3b my-card">
+                                    <h3 class="gwas-card-head"> Summary Statistics </h3>
+                                    <div class="gwas-card-body-one centralize">
+                                        <p class="mg-t-0">
+                                            Documentation and access to full summary statistics for GWAS Catalog studies
+                                            where available.
+                                        <p>
+                                        <div class="card-footer bd-t-0 pd-t-0">
+                                            <button class="tx-12 tx-uppercase gwas-card-btn"> FUll Summary Statistics
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div> -->
+                    </div>
+                </div>
+            </section>
+        </main>
+
+        <Footer/>
+    </div>
+</template>
+
+
+<script>
+export default {
+	async asyncData({ $content, params }) {
+		const article = await $content('blog', params.slug).fetch();
+
+		return { article }
+	}
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,61 @@
+<template>
+    <div>
+        <main class="gwas-container">
+            <div class="section section--gray100 gwas-banner">
+                <Header/>
+                <Banner/>
+            </div>
+
+            <section class="gwas-gray20 body-section">
+                <div class="row margin-top-25">
+
+                    <!-- <Sidebar/> -->
+
+                    <div class="column large-10 medium-12 large-offset-1" style="padding: 0 5px">
+                        <div class="row">
+                            <div v-for="article of articles" :key="article">
+                                <div class="column large-12 medium-12" style="padding: 0 5px; margin-bottom: 10px;">
+                                    <div class="card-2 card-body card-3b my-card">
+                                        <h3 class="gwas-card-head">{{ article.title }} 
+                                            <em style="float:right; font-size:11px; color:#347883;"> {{article.date}} By {{ article.author }} </em> 
+                                        </h3>
+                                        <div class="gwas-card-body-one" style="margin-top:-15px;">
+                                            <p class="mg-t-0">{{ article.description }} ... </p>
+                                            <p></p>
+                                            <div class="card-footer bd-t-0 pd-t-0" style="margin-top:20px;">
+                                                <nuxt-link :to="{ name: 'slug', params: { slug: article.slug } }">
+                                                      <button  class="tx-12 tx-uppercase gwas-card-btn"> Continue Reading >>> </button>
+                                                </nuxt-link>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
+
+        <Footer/>
+    </div>
+</template>
+
+<script>
+import Footer from "../components/Footer.vue";
+import Sidebar from '../components/Sidebar.vue';
+export default {
+  components: { Footer },
+  
+  async asyncData({ $content, params }) {
+    const articles = await $content("blog", params.slug)
+      .only(["title", "author", "date", "description", "img", "slug"])
+      .sortBy("createdAt", "desc")
+      .fetch();
+
+    return {
+      articles,
+    };
+  },
+};
+</script>
