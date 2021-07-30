fa876b0ffe91237dccfafbfc65805db9dc93a00d
sprintell <abayomi@sprintell.org>
2021-07-30 16:02:07 +0100

    set up global markdown image handler component

---
+++ components/global/ArticleImage.vue
@@ -0,0 +1,40 @@
+<template>
+  <img :src="imgSrc()" :alt="alt" class="article-image" />
+</template>
+
+<script>
+export default {
+  props: {
+    src: {
+      type: String,
+      required: true
+    },
+    alt: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    imgSrc() {
+      try {
+        return require(`~/assets/images/${this.src}`)
+      } catch (error) {
+        return null
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.article-image {
+  width: 50%;
+  border: 1px solid #B5E0E7;
+  display: block;
+  margin-top: 30px;
+  margin-bottom: 30px;
+  padding: 5px;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
