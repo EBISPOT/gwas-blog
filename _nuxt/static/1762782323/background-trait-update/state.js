f82e8ca807a01f33342f4d4b832aab2caba4fbec
sprintell <abayomi@sprintell.org>
2025-11-10 13:45:30 +0000

    7f7dc60

---
+++ _nuxt/static/1762782323/background-trait-update/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fgwas-blog\u002F_nuxt\u002Fstatic\u002F1762782323",layout:"default",error:a,serverRendered:true,routePath:"\u002Fbackground-trait-update",config:{_app:{basePath:"\u002Fgwas-blog\u002F",assetsPath:"\u002Fgwas-blog\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"cc23c9d5"}}}}(null));
