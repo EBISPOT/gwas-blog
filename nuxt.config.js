942ec151af9676dad3752834e4e8a2fe77016e13
Benjamin Wingfield <bwingfield@ebi.ac.uk>
2026-08-13 10:55:02 +0100

    update base URL

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
   router: {
-    base: '/gwas-blog/'
+    base: '/blog/'
   },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
